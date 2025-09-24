b527d408615c48176db01dad28a4bb18ce1f3a2a
samyak-the-dev <samyakthedev@gmail.com>
2025-09-24 23:01:21 +0530

    SW location error fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -287,7 +287,7 @@ document.addEventListener("DOMContentLoaded", () => {
 if ('serviceWorker' in navigator && 'caches' in window) {
   window.addEventListener('load', async () => {
     try {
-      const registration = await navigator.serviceWorker.register('/sw.js');
+      const registration = await navigator.serviceWorker.register('..So/sw.js');
       console.log('ServiceWorker registered successfully');
       registration.addEventListener('updatefound', () => {
         const newWorker = registration.installing;
